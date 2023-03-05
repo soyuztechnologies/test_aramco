002fdc207931221af2bcaf695205e5d5848b84d5
Punniyakoti Chandrasekaran <chandrasekaran.punniyakoti@jhah.com>
2023-03-05 12:58:04 +0000

    second change

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,6 +3,7 @@ sap.ui.define([
 ], function (BaseController) {
 	'use strict';
 	// this is a comment from Chandra
+	// added a new line
 	return BaseController.extend("emc2.sd.products.controller.App", {
 		//This is the constructor of the class
 		//where we can put all the initialization code
