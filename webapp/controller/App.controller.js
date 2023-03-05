7f6f772769fd92b8c16fcbb98a142658b70a8997
Punniyakoti Chandrasekaran <chandrasekaran.punniyakoti@jhah.com>
2023-03-05 12:55:37 +0000

    Chandra changes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,14 @@
 sap.ui.define([
-    'emc2/sd/products/controller/BaseController'
-], function(BaseController) {
-    'use strict';
-    return BaseController.extend("emc2.sd.products.controller.App",{
-        //This is the constructor of the class
-        //where we can put all the initialization code
-        onInit: function(){
-            // alert("welcome to my app");
-            // this is testing Rakesh change
-        }
-    });
+	'emc2/sd/products/controller/BaseController'
+], function (BaseController) {
+	'use strict';
+	// this is a comment from Chandra
+	return BaseController.extend("emc2.sd.products.controller.App", {
+		//This is the constructor of the class
+		//where we can put all the initialization code
+		onInit: function () {
+			// alert("welcome to my app");
+			// this is testing Rakesh change
+		}
+	});
 });
